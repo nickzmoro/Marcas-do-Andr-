bc56d28f1165750cf3752a8f26c8e7aa67effb6f
nickzmoro <nicolasmororuiz@gmail.com>
2024-12-10 19:48:54 -0300

    added função no js com o link "saiba mais" em detalhesGranulato / responsividade

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// formulário
 const form = document.querySelector(".formulario");
 const mascara = document.querySelector(".mask");
 
@@ -8,5 +9,25 @@ function mostrarForm() {
 
 function esconderForm() {
     form.style.bottom = "-50%"
+    mascara.style.visibility = "hidden"
+}
+
+// detalhes das marcas
+const detalhesGranulato = document.querySelector(".detalhesGranulato");
+const granulatoContent = document.querySelector(".granulato-content");
+
+function mostrarDetalhesGranulato() {
+    detalhesGranulato.style.zIndex = "98"
+    granulatoContent.style.transform = "translate(-50%, 4%)"
+    mascara.style.visibility = "visible"
+}
+
+const btnFechar = document.querySelector(".btn-fechar");
+const allDetalhesMarcas = document.querySelector(".detalhes-marcas");
+const allDetalhesContent = document.querySelector(".detalhes-content");
+
+function fechaDetalhes() {
+    allDetalhesMarcas.style.zIndex = "-1"
+    allDetalhesContent.style.transform = "translate(-200%, 4%)"
     mascara.style.visibility = "hidden"
 }
